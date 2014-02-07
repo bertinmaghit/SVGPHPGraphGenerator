bb6632d7eab4405676f441c514f944b412691f9d
Maxy.fr <guillaume@maxy.fr>
2014-02-07 01:28:53 +0100

    Ajout multi courbes + couleurs
    Il est maintenant possible d’inclure plusieurs courbes sur un seul
    graphique + La couleur de chacune des courbes est maintenant
    personnalisable.

--- exemple.php
+++ exemple.php
@@ -17,10 +17,18 @@
 		'Mai' => 167,
 		'Juin' => 117);
 
+	$data2 = array(
+		'Lundi' => array(123, 765),
+		'Mardi' => array(432, 612),
+		'Mercredi' => array(69, 234),
+		'Jeudi' => array(451, 90),
+		'Vendredi' => array(54, 80)
+		);
+
 	$graph = new graph(700, 300, $data, true);
 	echo $graph->graphLignes();
 
-	$graph = new graph(700, 300, $data, false);
+	$graph = new graph(700, 300, $data2, false, array('red', 'green'));
 	echo $graph->graphLignes();
 	?>
 	<script src="jquery-2.1.0.min.js"></script>
--- svgraph.class.php
+++ svgraph.class.php
@@ -1,14 +1,18 @@
 <?php
 class graph
 {
-	protected $largeur, $hauteur, $data = array(), $basZero;
+	protected $largeur, $hauteur, $data = array(), $basZero, $couleursChemins;
+	protected $margeBas, $margeGauche, $ecartColonnes, $maxY, $minY;
 
-	public function __construct($largeur, $hauteur, $data, $basZero = false)
+	public function __construct($largeur, $hauteur, $data, $basZero = false, $couleursChemins = array('blue'))
 	{
 		$this->largeur = $largeur;
 		$this->hauteur = $hauteur;
 		$this->data = $data;
 		$this->basZero = $basZero;
+		$this->margeBas = 50;
+		$this->margeGauche = 70;
+		$this->couleursChemins = $couleursChemins;
 	}
 
 	public function graphLignes()
@@ -16,67 +20,89 @@ class graph
 		$verifErreur = $this->verifErreur();
 		if($verifErreur != 'ok'){ return $verifErreur; }
 
-		$margeGauche = 70;
-		$margeBas = 50;
-		$maxY = $this->calculValeurYMax();
-		$minY = $this->calculValeurYMin();
+		$this->maxY = $this->calculValeurYMax();
+		$this->minY = $this->calculValeurYMin();
 		$nombreLignesHorizontal = 10;
-		$ecartColonnes = ($this->largeur-$margeGauche)/(count($this->data)-1);
+		$this->ecartColonnes = ($this->largeur-$this->margeGauche)/(count($this->data)-1);
 
 		$result = '<svg style="width:'.$this->largeur.'; height:'.$this->hauteur.';">';
 
 		//Rectangle :
-		$result .= '<rect x="'.$margeGauche.'" y="0" width="'.($this->largeur-$margeGauche).'" height="'.($this->hauteur-$margeBas).'" class="rectangle"/>';
+		$result .= '<rect x="'.$this->margeGauche.'" y="0" width="'.($this->largeur-$this->margeGauche).'" height="'.($this->hauteur-$this->margeBas).'" class="rectangle"/>';
 
 		//Lignes horizontales :
 		for ($i = 1; $i < $nombreLignesHorizontal; $i++) { 
-			if(!$this->basZero){ $val = ($i*($maxY-$minY))/$nombreLignesHorizontal; $val += $minY; }
-			else{ $val = ($i*$maxY)/$nombreLignesHorizontal; }
+			if(!$this->basZero){ $val = ($i*($this->maxY-$this->minY))/$nombreLignesHorizontal; $val += $this->minY; }
+			else{ $val = ($i*$this->maxY)/$nombreLignesHorizontal; }
 			$val = round($val);
-			$result .= '<text x="'.($margeGauche-5).'" y="'.($this->calculPositionY($val, $margeBas)+12).'" text-anchor="end" class="text">'.$val.'</text>';
-			$result .= '<line x1="'.$margeGauche.'" x2="'.$this->largeur.'" y1="'.($this->calculPositionY($val, $margeBas)).'" y2="'.$this->calculPositionY($val, $margeBas).'" class="ligneFond"/>';
+			$result .= '<text x="'.($this->margeGauche-5).'" y="'.($this->calculPositionY($val, $this->margeBas)+12).'" text-anchor="end" class="text">'.$val.'</text>';
+			$result .= '<line x1="'.$this->margeGauche.'" x2="'.$this->largeur.'" y1="'.($this->calculPositionY($val, $this->margeBas)).'" y2="'.$this->calculPositionY($val, $this->margeBas).'" class="ligneFond"/>';
 		}
 
 		//Texte :
-		$result .= '<text x="'.($margeGauche-5).'" y="12" text-anchor="end" class="text">'.$maxY.'</text>';
-		$result .= '<text x="'.($this->largeur).'" y="'.($this->hauteur-$margeBas+30).'" text-anchor="end" class="text" style="background-color:white;" id="tooltip"></text>';
+		$result .= '<text x="'.($this->margeGauche-5).'" y="12" text-anchor="end" class="text">'.$this->maxY.'</text>';
+		$result .= '<text x="'.($this->largeur).'" y="'.($this->hauteur-$this->margeBas+30).'" text-anchor="end" class="text" style="background-color:white;" id="tooltip"></text>';
 
 		//- Texte bas :
 		$i = 0;
 		foreach ($this->data as $key => $value) {
 			$textanchor = "middle";
 			if($i == count($this->data)-1){ $textanchor = "end"; }
-			$result .= '<text x="'.(($ecartColonnes*$i)+$margeGauche).'" y="'.($this->hauteur-$margeBas+15).'" text-anchor="'.$textanchor.'" class="text">'.$key.'</text>'; $i++;
+			$result .= '<text x="'.(($this->ecartColonnes*$i)+$this->margeGauche).'" y="'.($this->hauteur-$this->margeBas+15).'" text-anchor="'.$textanchor.'" class="text">'.$key.'</text>'; $i++;
 		}
 
 		//Chemin :
-		$chemin = '';
-		$cercles = '';
-		$i = 0;
-		foreach ($this->data as $key => $value) {
-			if($i == 0){ $chemin .= 'M '; }
-			else{ $chemin .= 'L '; }
-
-			$x = (($ecartColonnes*$i)+$margeGauche);
-			$y = $this->hauteur - $margeBas - ((($this->hauteur - $margeBas)*$value)/$maxY); //Bas = 0
-			$y = $this->calculPositionY($value, $margeBas);
-
-			//Décalage premier et dernier points :
-			if($i == 0){ $x += 6; }
-			if($i == count($this->data)-1){ $x -= 6; }
-
-			$chemin .= $x.' '.$y.' ';
-
-			$cercles .= '<circle cx="'.$x.'" cy="'.$y.'" r="4" title="'.$key.' : '.$value.'" class="cercle"/>';
-
-			$i++;
-		}
-		$result .= '<path d="'.$chemin.'" class="chemin"/>';
-		//$result .= '<path d="'.$chemin.' L '.$this->largeur.' '.($this->hauteur-$margeBas).' L '.$margeGauche.' '.($this->hauteur-$margeBas).'" class="cheminFond"/>';
-		$result .= $cercles;
+		$result .= $this->traceChemin();
 
 		//Résultat :
 		$result .= '</svg>';
+
+		return $result;
+	}
+
+	private function traceChemin()
+	{
+		$chemin = array();
+		$cercles = array();
+		$result = '';
+		$i = 0;
+		foreach ($this->data as $key => $value) {
+
+			if(!is_array($value)){ $value = array($value); }
+
+			//Parcours toutes les valeurs Y de la position X actuelle
+			foreach ($value as $k => $val)
+			{
+				if($i == 0){ $chemin[$k] = 'M '; $cercles[$k] = ''; }
+				else{ $chemin[$k] .= 'L '; }
+
+				$x = (($this->ecartColonnes*$i)+$this->margeGauche);
+				$y = $this->calculPositionY($val, $this->margeBas);
+
+				//Décalage premier et dernier points :
+				if($i == 0){ $x += 6; }
+				if($i == count($this->data)-1){ $x -= 6; }
+
+				$chemin[$k] .= $x.' '.$y.' ';
+
+				$cc = '';
+				if(isset($this->couleursChemins[$k])){ $cc = 'style="stroke:'.$this->couleursChemins[$k].';"'; }
+				$cercles[$k] .= '<circle cx="'.$x.'" cy="'.$y.'" r="4" title="'.$key.' : '.$val.'" class="cercle" '.$cc.'/>';	
+			}
+
+			$i++;
+		}
+
+		foreach ($chemin as $key => $chem) {
+			$cc = '';
+			if(isset($this->couleursChemins[$key])){ $cc = 'style="stroke:'.$this->couleursChemins[$key].';"'; }
+
+			$result .= '<path d="'.$chem.'" class="chemin" '.$cc.'/>';
+		}
+		
+		foreach ($cercles as $cerc) {
+			$result .= $cerc;
+		}
 
 		return $result;
 	}
@@ -93,21 +119,26 @@ class graph
 		else{ return json_encode(array('erreur' => true, 'msg' => $msg, 'data' => $this->data)); }
 	}
 
-	private function calculPositionY($valeur, $margeBas)
+	private function calculPositionY($valeur)
 	{
 		//Début axe y à zéro ou à la valeur min des données
-		if($this->basZero){ return $this->hauteur - $margeBas - ((($this->hauteur - $margeBas)*$valeur)/$this->calculValeurYMax()); }
-		else{ return $this->hauteur - $margeBas - (($valeur-$this->calculValeurYMin())*($this->hauteur - $margeBas))/($this->calculValeurYMax()-$this->calculValeurYMin()); }
+		if($this->basZero){ return $this->hauteur - $this->margeBas - ((($this->hauteur - $this->margeBas)*$valeur)/$this->calculValeurYMax()); }
+		else{ return $this->hauteur - $this->margeBas - (($valeur-$this->calculValeurYMin())*($this->hauteur - $this->margeBas))/($this->calculValeurYMax()-$this->calculValeurYMin()); }
 	}
 
 	private function calculValeurYMax()
 	{
 		$max = 0;
 
-		foreach ($this->data as $key => $value) {
-			if($max < $value){ $max = $value; }
+		foreach ($this->data as $value) {
+			if(is_array($value))
+			{
+				foreach ($value as $val) {
+					if($max < $val){ $max = $val; }
+				}
+			}
+			else{ if($max < $value){ $max = $value; } }
 		}
-
 		return $max;
 	}
 
@@ -115,17 +146,28 @@ class graph
 	{
 		$min = 999999999999999999;
 
-		foreach ($this->data as $key => $value) {
-			if($min > $value){ $min = $value; }
+		foreach ($this->data as $value) {
+			if(is_array($value))
+			{
+				foreach ($value as $val) {
+					if($min > $val){ $min = $val; }
+				}
+			}
+			else{ if($min > $value){ $min = $value; } }
 		}
-
 		return $min;
 	}
 
 	private function verifErreurNumerique()
 	{
-		foreach ($this->data as $key => $value) {
-			if(!is_numeric($value)){ return true; }
+		foreach ($this->data as $value) {
+			if(is_array($value))
+			{
+				foreach ($value as $val) {
+					if(!is_numeric($val)){ return true; }
+				}
+			}
+			else{ if(!is_numeric($value)){ return true; } }
 		}
 
 		return false;
